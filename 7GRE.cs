682e7d255b95db906abfe3b83d44927cfa883c56
Lyviaff <33584326+Lyviaff@users.noreply.github.com>
2018-01-10 15:34:01 +0100

    Update 7GRE.cs

--- 7GRE.cs
+++ 7GRE.cs
@@ -266,7 +266,7 @@ namespace _7G_Replay_Editor
 
         private void lyviaffToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            DialogResult result = MessageBox.Show("This program has been developed by Lyviaff. Do you want to show him Twitter?", "Lyviaff", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+            DialogResult result = MessageBox.Show("This program has been developed by Lyviaff. Do you want to show his Twitter?", "Lyviaff", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
             if (result == DialogResult.Yes)
             {
                 System.Diagnostics.Process.Start("https://www.Twitter.com/Lyviaff");
